faa9cac67efffdccd754d7b306daac0505af25fd
DIVYANSHSAXENA23 <divyanshsaxena69@gmail.com>
2025-05-22 18:32:30 +0530

    init

---
+++ java/sum of 2 numbers/main.java
@@ -0,0 +1,13 @@
+
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println( "sum of two numbers is :");
+        int num1=2;
+        int num2=5;
+        int num3=7;
+        int sum;
+        sum= num1+ num2+ num3;
+        System.out.println(sum);
+    }
+}
