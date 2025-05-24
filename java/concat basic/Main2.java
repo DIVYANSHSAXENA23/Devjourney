28fb052f70d243736ecf1ad90e5a6b2e04f738b8
DIVYANSHSAXENA23 <divyanshsaxena69@gmail.com>
2025-05-24 18:19:16 +0530

    init

---
+++ java/concat basic/Main2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main2 {
+    
+
+    public static void main(String [] args){
+        Scanner d=new Scanner(System.in);
+        System.out.println("enter your name");
+        String b=d.nextLine();
+        System.out.println("enter marks");
+        int z=d.nextInt();
+        
+        System.out.print("marks obtained by " + b + " are " + z);
+    }
+    }
+
